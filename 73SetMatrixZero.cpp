daa528facf563d6a7504b8ec6f5a664b926fc3a3
Naman Shah <141851295+16naman11@users.noreply.github.com>
2024-06-19 14:17:15 +0530

    set matrix 0

---
+++ 73SetMatrixZero.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        int rows = matrix.size();
+        int cols = matrix[0].size();
+        vector<bool> rowZero(rows, false);
+        vector<bool> colZero(cols, false);
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (matrix[i][j] == 0) {
+                    rowZero[i] = true;
+                    colZero[j] = true;
+                }
+            }
+        }
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (rowZero[i] || colZero[j]) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+        cout << "[";
+        for (int i = 0; i < rows; i++) {
+            cout << "[";
+            for (int j = 0; j < cols; j++) {
+                cout << matrix[i][j];
+                if (j != cols - 1) {
+                    cout << ",";
+                }
+            }
+            cout << "]";
+            if (i != rows - 1) {
+                cout << ",";
+            }
+        }
+        cout << "]";
+    }
+};
